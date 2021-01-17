a39e02b1c90dcc9ece7a4121b13bcb4f598f07e2
Greg Oliver <golive@microsoft.com>
2021-01-17 11:48:40 +0000

    initial commit

---
+++ MyActorClient/Program.cs
@@ -0,0 +1,44 @@
+﻿using Dapr.Actors;
+using Dapr.Actors.Client;
+using MyActor.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace MyActorClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                await InvokeActorMethodWithRemotingAsync(i);
+            }
+
+            while (!Console.KeyAvailable)
+            {
+                System.Threading.Thread.Sleep(10);
+            }
+        }
+        static async Task InvokeActorMethodWithRemotingAsync(int actorId)
+        {
+            var actorType = "MyActor";      // Registered Actor Type in Actor Service
+            var actorID = new ActorId(actorId.ToString());
+
+            // Create the local proxy by using the same interface that the service implements
+            // By using this proxy, you can call strongly typed methods on the interface using Remoting.
+            var proxy = ActorProxy.Create<IMyActor>(actorID, actorType);
+            var response = await proxy.SetDataAsync(new MyData()
+            {
+                PropertyA = "ValueA",
+                PropertyB = "ValueB",
+            });
+            Console.WriteLine(response);
+
+            await proxy.RegisterReminder("MyFirstReminder");
+
+            var savedData = await proxy.GetDataAsync();
+            Console.WriteLine(savedData);
+        }
+    }
+}
---
+++ MyActorService/Program.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Dapr.Actors.AspNetCore;
+
+namespace MyActorService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseActors(options => 
+                    {
+                        options.Actors.RegisterActor<MyActor>();
+                    });
+                });
+    }
+}
